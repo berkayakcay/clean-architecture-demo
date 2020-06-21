7fe3a7946cd4083e4aca1fe344437479f38b56fb
Berkay AKÇAY <berkayakcay.m@gmail.com>
2020-06-21 22:49:15 +0300

    add semantic-ui-react

--- src/client-app/src/App.tsx
+++ src/client-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Header, Icon, List } from "semantic-ui-react";
 import "./App.css";
 import axios from "axios";
 
@@ -8,21 +8,13 @@ function App() {
     console.log(response);
   });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as="h2" icon="users" content="Insurers" />
+      <List>
+        <List.Item>Apples</List.Item>
+        <List.Item>Pears</List.Item>
+        <List.Item>Oranges</List.Item>
+      </List>
     </div>
   );
 }
